57b5ed87d792dde168a183250736f477404a313e
Qishen Wu <22854142+wuqs-net@users.noreply.github.com>
2020-02-01 10:03:03 -0500

    Remove unused CSS styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import WelcomePage from './welcome_page/index';
 
 ReactDOM.render(<WelcomePage />, document.getElementById('root'));
